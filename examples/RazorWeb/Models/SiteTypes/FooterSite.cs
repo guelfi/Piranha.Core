672d761379457614cb10a7d93d2bde577bc45ce9
Henrique Platero <henrique.platero@lis2b.com.br>
2023-12-13 12:05:12 -0300

    Adicionando footer

---
+++ examples/RazorWeb/Models/SiteTypes/FooterSite.cs
@@ -0,0 +1,21 @@
+﻿using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Models;
+using Piranha.Extend.Fields;
+using Microsoft.AspNetCore.Components.Routing;
+
+[SiteType(Title = "Footer Site")]
+public class FooterSite : SiteContent<FooterSite>
+{
+    [Region(Title = "Footer", Display = RegionDisplayMode.Setting)]
+    public Footer FooterContents { get; set; }
+}
+
+public class Footer
+{
+    [Field(Title = "Footer logo")]
+    public ImageField Logo { get; set; }
+
+    [Field(Title = "Footer text")]
+    public HtmlField Text { get; set; }
+}
