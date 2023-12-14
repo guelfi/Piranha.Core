22a7e6af095a2e11624752695412fbd3443e08b3
Henrique Platero <henrique.platero@lis2b.com.br>
2023-12-14 16:20:36 -0300

    Ajustando Footer

---
+++ examples/RazorWeb/Pages/_Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorWeb.Pages
+{
+    public class _FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
