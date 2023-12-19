d54ef35d54bccfedb2ec97ae9576d2451b5652ec
Marco.Guelfi.LIS <118921476+MarcoGuelfiLis@users.noreply.github.com>
2023-12-19 14:47:15 -0300

    configuração de acesso a paginas

---
+++ examples/RazorWeb/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace RazorWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add custom policies
+            services.AddAuthorization(o =>
+            {
+                // Read secured posts
+                o.AddPolicy("ReadSecuredPosts", policy =>
+                {
+                    policy.RequireClaim("ReadSecuredPosts", "ReadSecuredPosts");
+                });
+            });
+        }
+
+
+    }
+}
---
+++ examples/RazorWeb/Utils/Utils.cs
@@ -0,0 +1,51 @@
+﻿namespace RazorWeb
+{
+    public class Utils
+    {
+        public static string GetImage(string serverPath, string nameImage)
+        {
+            string folder = "wwwroot\\uploads\\";
+            string pathImage = LocalizeImage(serverPath, folder, nameImage);
+
+            if (pathImage != null)
+            {
+                string folderPath = Path.GetDirectoryName(pathImage).Replace("\\", "/").Replace("wwwroot", "/");
+
+                pathImage = "http://" + serverPath + $"{folderPath.Replace("//uploads", "/uploads")}/" + nameImage;
+            }
+            return pathImage;
+        }
+
+        public static string LocalizeImage(string serverPath, string folder, string nameImage)
+        {
+            try
+            {
+                string[] archives = Directory.GetFiles(folder);
+
+                foreach (string archive in archives)
+                {
+                    if (Path.GetFileName(archive).Equals(nameImage, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return archive;
+                    }
+                }
+
+                string[] subfolders = Directory.GetDirectories(folder);
+
+                foreach (string subfolder in subfolders)
+                {
+                    string path = LocalizeImage(serverPath, subfolder, nameImage);
+                    if (path != null)
+                    {
+                        return path;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"   Erro ao acessar diretorio: {ex}   ";
+            }
+            return null;
+        }
+    }
+}
